c0746591af87d165797315374c2f651322f680d3
senuri_s <senuri.intern@gmail.com>
2019-10-27 14:07:48 -0700

    Calculate sum of two numbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,5 +35,10 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
+    int x =5;
+    int y = 9;
+    int z = x+y;
+
+    System.out.println("5+9 = "+ z);
   }
 }
