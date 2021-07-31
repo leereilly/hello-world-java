ae6faf2aa7761c226a4ba419ce0697d2426d5879
AERR <email2arungokulabalan@gmail.com>
2021-07-31 20:19:09 +0530

    missed semicolon has been added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, Dhuniya");
     System.out.println("Hello, Ulagamae");
-    System.out.println("Hello, Welcome to Earth")
-    System.out.println("Hello, Bilkul nahin")
+    System.out.println("Hello, Welcome to Earth");
+    System.out.println("Hello, Bilkul nahin");
     System.out.println("Hello, Zindagi Rocks");
   }
 }
